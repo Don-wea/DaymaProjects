3348d8b523622df33881bb5c3de0f8bb53d809f8
Don-Wea <podlech@live.com.ar>
2024-09-17 20:21:37 -0300

    mi primer git

---
+++ Sistema_gestor_de_inventario.py
@@ -0,0 +1,33 @@
+#"hola mundo"(print);
+
+
+archivo = "inventario.csv"
+f = open(archivo,"r")
+datos = f.readlines()
+NombreProductos = []
+NombreProductos = datos[0].rstrip().split(",")
+
+CantidadProductos = []
+CantidadProductos = datos[1].rstrip().split(",")
+
+ValorProductos = []
+ValorProductos = datos[2].rstrip().split(",")
+
+print(NombreProductos)
+print(CantidadProductos)
+print(ValorProductos)
+
+#print(datos)
+f.close()
+
+def AgregarProducto():
+    print("AGREGADO")
+    
+    
+
+# 1. leer lo que ya existe
+# guardar en un array temporal los datos del inventario
+
+# 2. modificar los datos del array temporal
+
+# 3. cargar los archivos actualizados en orden en modo escritura
---
+++ basemain.sql
@@ -0,0 +1,22 @@
+-- create database tataBase
+
+-- using tataBase
+
+-- --hola, esto es solo para probar el live session by tata
+
+-- create table products(
+--     int produtc_id primary key,
+--     character name (15),
+--     character description(255),
+--     character tipoProducto(15)
+
+
+-- );
+
+-- create table usuario(
+--     int usuario_id primary key,
+--     character nombre,
+
+
+
+-- );
